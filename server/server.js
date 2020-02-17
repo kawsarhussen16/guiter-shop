4e476f6e02e35621e89cfd136f535032a0d83620
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2020-02-17 18:56:19 -0500

    User login and registration imported in server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const User = require("./router/User.js");
+require("dotenv").config();
+
+const app = express();
+
+mongoose
+	.connect(process.env.DATABASE)
+	.then(() => console.log("Db connected"))
+	.catch(err => console.log(err));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+///////// User Router ///////
+app.use("/api/users", User);
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
