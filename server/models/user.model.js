3e321a23d4ad2da1fbeecea178dab761db1d16ba
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2020-02-17 18:24:22 -0500

    User model implemented for password hashing and register

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userSchema = mongoose.Schema({
 	email: {
 		type: String,
@@ -39,6 +41,19 @@ const userSchema = mongoose.Schema({
 	}
 });
 
+userSchema.pre("save", function(next) {
+	if (!this.isModified("password")) {
+		return next();
+	}
+	bcrypt.genSalt(10, (err, salt) => {
+		if (err) return next(err);
+		bcrypt.hash(this.password, salt, (err, hash) => {
+			if (err) return next(err);
+			this.password = hash;
+			next();
+		});
+	});
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
