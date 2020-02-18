541d5ed5555831870175c1dec1361345ac6c86db
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2020-02-17 23:06:13 -0500

    necessary packages installed and unnecessary file deleted

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./resources/styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Routes from "./Routes";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+	<BrowserRouter>
+		<Routes />
+	</BrowserRouter>,
+	document.getElementById("root")
+);
