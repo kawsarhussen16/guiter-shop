f59ca8a1953bef6cc241d5bf3636ffb1d7995a1c
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-02-25 15:12:11 -0500

    Login-Registration router created

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home/index";
 import Layout from "./hoc/layout";
+import LoginRegister from "./components/Login-register";
 const Routes = () => {
 	return (
 		<Layout>
 			<Switch>
 				<Route path="/" exact component={Home} />
+				<Route
+					path="/register_login"
+					exact
+					component={LoginRegister}
+				/>
 			</Switch>
 		</Layout>
 	);
