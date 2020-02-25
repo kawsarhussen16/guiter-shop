eb34ad35690e9135b72cecbc8de056d6c56756dd
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-02-25 15:11:33 -0500

    Few utility function implemented for from validaton

---
+++ client/src/utils/Form/formActions.js
@@ -0,0 +1,98 @@
+export const validate = (element, formData = []) => {
+	let error = [true, ""];
+
+	if (element.validation.email) {
+		const valid = /\S+@\S+\.\S+/.test(element.value);
+		const message = `${!valid ? "Must be a valid email" : ""}`;
+		error = !valid ? [valid, message] : error;
+	}
+
+	if (element.validation.confirm) {
+		const valid =
+			element.value.trim() ===
+			formData[element.validation.confirm].value;
+		const message = `${!valid ? "Passwords do not match" : ""}`;
+		error = !valid ? [valid, message] : error;
+	}
+
+	if (element.validation.required) {
+		const valid = element.value.trim() !== "";
+		const message = `${!valid ? "This field is required" : ""}`;
+		error = !valid ? [valid, message] : error;
+	}
+
+	return error;
+};
+
+export const update = (element, formData, formName) => {
+	const newFormdata = {
+		...formData
+	};
+	const newElement = {
+		...newFormdata[element.id]
+	};
+
+	newElement.value = element.event.target.value;
+
+	if (element.blur) {
+		let validData = validate(newElement, formData);
+		newElement.valid = validData[0];
+		newElement.validationMessage = validData[1];
+	}
+
+	newElement.touched = element.blur;
+	newFormdata[element.id] = newElement;
+
+	return newFormdata;
+};
+
+export const generateData = (formData, formName) => {
+	let dataToSubmit = {};
+
+	for (let key in formData) {
+		if (key !== "confirmPassword") {
+			dataToSubmit[key] = formData[key].value;
+		}
+	}
+
+	return dataToSubmit;
+};
+
+export const isFormValid = (formData, formName) => {
+	let formIsValid = true;
+
+	for (let key in formData) {
+		formIsValid = formData[key].valid && formIsValid;
+	}
+	return formIsValid;
+};
+
+export const populateOptionFields = (formData, arrayData = [], field) => {
+	const newArray = [];
+	const newFormdata = { ...formData };
+
+	arrayData.forEach(item => {
+		newArray.push({ key: item._id, value: item.name });
+	});
+
+	newFormdata[field].config.options = newArray;
+	return newFormdata;
+};
+
+export const resetFields = (formData, formName) => {
+	const newFormdata = { ...formData };
+
+	for (let key in newFormdata) {
+		if (key === "images") {
+			newFormdata[key].value = [];
+		} else {
+			newFormdata[key].value = "";
+		}
+
+		newFormdata[key].valid = false;
+		newFormdata[key].touched = false;
+		newFormdata[key].validationMessage = "";
+	}
+
+	return newFormdata;
+};
